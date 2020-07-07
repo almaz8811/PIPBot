951c6ac1f7655f0b9903d130d843f507a1012a55
AlMaz <almaz8811@gmail.com>
2020-07-07 18:11:03 +0300

    222

--- mongoconnect.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('mongodb+srv://almaz8811:almaz084284@pipbot-s1ibg.mongodb.net/<dbname>?retryWrites=true&w=majority')
-db = client.test
-db.chats.insert_one({'user': 'Александр', 'phone': '+79383575333'})
--- settings.py
+++
@@ -1,4 +0,0 @@
-TG_TOKEN = '1058830205:AAE08ohVqYmLr4lnb_LzolwhUqiSs8vTD1A'
-TG_API_URL = 'https://telegg.ru/orig/bot'
-MONGODB_LINK = 'mongodb+srv://almaz8811:almaz084284@pipbot-s1ibg.mongodb.net/<dbname>?retryWrites=true&w=majority'
-MONGO_DB = 'pip_bot'
