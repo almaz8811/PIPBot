d0d8a9e10d46ea70347497f0f51fa6b6cac3c57d
AlMaz <almaz8811@gmail.com>
2020-07-11 23:01:32 +0300

    1

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(message)s',
 
 # Создаем функцию main(), которая соединяется с сервисом Telegram
 def main():
-    my_bot = Updater(os.environ.get('TG_TOKEN'), os.environ.get('TG_API_URL'), use_context = True)
+    my_bot = Updater(os.environ['TG_TOKEN'], os.environ['TG_API_URL'], use_context = True)
     logging.info('Start Bot')
     my_bot.dispatcher.add_handler(CommandHandler('start', sms))
     my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('Картинки'), send_meme))
