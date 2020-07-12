20b683639a9766159b8a9e1069e85446e6034731
AlMaz <almaz8811@gmail.com>
2020-07-12 04:46:46 +0300

    Fix

--- handlers.py
+++ handlers.py
@@ -31,7 +31,6 @@ def send_meme(bot, update):
         chat_id = bot.message.chat.id,
         photo = open(picture, 'rb'),
         reply_markup = inl_keyboard) # Отправляем картинку и inline клавиатуру
-    print(msg)
     save_file_id(mdb, picture, msg)
     # update.bot.send_photo(chat_id = bot.message.chat_id, photo = open(picture, 'rb'), reply_markup = inl_keyboard) # Отправляем картинку
 
